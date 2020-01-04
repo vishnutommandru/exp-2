fc985601bad14e1d51083a92c23c2f968784b980
vishnutommandru <46985936+vishnutommandru@users.noreply.github.com>
2020-01-04 15:02:48 +0530

    Update AreaOfRectangle.java

--- AreaOfRectangle.java
+++ AreaOfRectangle.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 class AreaOfRectangle {
    public static void main (String[] args)
    {
-	   Scanner scanner = new Scanner(System.in);
-	   System.out.println("Enter the length of Rectangle:");
-	   double length = scanner.nextDouble();
-	   System.out.println("Enter the width of Rectangle:");
-	   double width = scanner.nextDouble();
+	   
+	    double length = 10;
+	   double width = 10;
 	   //Area = length*width;
 	   double area = length*width;
 	   System.out.println("Area of Rectangle is:"+area);
